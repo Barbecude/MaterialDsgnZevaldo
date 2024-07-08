f4998c5ccec21a15862d3eaebd468a089ab8f204
Zevaldo <92206730+Barbecude@users.noreply.github.com>
2024-07-08 21:15:56 +0700

    iygyufiy

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+/*ACTIVE LINK NAVIGATION*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute('id')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    } else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive)
+/*SMOOTH SCROLL*/
+$('a[href*="#"]').on('click', function (e) {
+  e.preventDefault()
+
+  $('html, body').animate(
+    {
+      scrollTop: $($(this).attr('href')).offset().top,
+    },
+    374,
+    'linear'
+  )
+})
